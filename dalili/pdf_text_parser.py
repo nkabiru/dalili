be377aadecd406bf1b923b25b7acea154dcf5bfe
Munyoki Kilyungi <me@bonfacemunyoki.com>
2023-08-17 23:31:49 +0300

    Swap 'fitz' with 'pdftotext', buffer reads/writes instead of files
    * dalili/__main__.py: Delete imports json, os, re,
    convert_url_to_filename.  Import io and pdftotext.
    <FOLDER, SUBFOLDERS>: Delete.
    (pdf_to_text, parse_text_file, to_json, to_stdout): Ditto.
    (fetch_interruptions): New function
    * dalili/pdf_text_parser.py (convert_url_to_filename): Delete
    
    Signed-off-by: Munyoki Kilyungi <me@bonfacemunyoki.com>

--- dalili/pdf_text_parser.py
+++ dalili/pdf_text_parser.py
@@ -20,18 +20,3 @@ def scrape_link_from_webpage(webpage: str) -> str:
     if link and not link.startswith("https://"):
         link = "https://" + link
     return link
-
-
-def convert_url_to_filename(url: str) -> str:
-    """Given a url, return a properly formatted filename E.g
-    interruptions_2023-05-11"""
-    filename, url = "", urlparse(url)
-    date_match = re.search(
-        r'(((0[1-9])|([12][0-9])|(3[01])).((0[1-9])|(1[0-2])).(\d{4}))',
-        filename:=unquote(url.path).rpartition("/")[-1]
-    )
-
-    if date_match:
-        filename = f"interruptions_\
-{datetime.strptime(date_match[0], '%d.%m.%Y').strftime('%Y-%m-%d')}"
-    return filename
