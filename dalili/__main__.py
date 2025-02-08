44c560550511339e8c63b9649648694588426d59
Njenga Kabiru <njengakabiru@gmail.com>
2025-02-08 11:11:58 +0300

    Create the pdfs directory and download code

--- dalili/__main__.py
+++ dalili/__main__.py
@@ -1,3 +1,4 @@
+import os
 import re
 
 import requests
@@ -19,15 +20,47 @@ def scrape_link_from_webpage(url):
         return None
 
 
-def download_pdf(url):
-# TODO: Download the pdf for parsing.
+def create_pdf_dir() -> None:
+    """
+    Create the dir for holding the files
+    """
+    if not os.path.exists("pdfs"):
+        os.mkdir("pdfs")
+        print("Folder created")
 
 
-def main():
-    latest_link = scrape_link_from_webpage(
-        "https://kplc.co.ke/customer-support#powerschedule"
-    )
+def download_pdf(url) -> bool:
+    """
+    Downloads the pdf into the pdfs folder.
+    """
+    filename = url.split("/")[-1]
+    filepath = os.path.join("pdfs", filename)
 
+    # Check if file already exists
+    if os.path.exists(filepath):
+        return True
+
+    # TODO: Check if existing file is complete.
+
+    response = requests.get(url, timeout=5)
+
+    if response.status_code == requests.codes.ok:
+        with open(filepath, mode="wb") as file:
+            file.write(response.content)
+        print("File downloaded")
+        return True
+
+    else:
+        print("File not downloaded")
+        return False
+
+
+def main() -> None:
+    url = scrape_link_from_webpage("https://kplc.co.ke/customer-support#powerschedule")
+    print(f"Link obtained: {url}")
+
+    create_pdf_dir()
+    download_pdf(url)
 
 
 if __name__ == "__main__":
